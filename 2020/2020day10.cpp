9cec171b3af457ba37274a27441be8ab9478cfbb
Ben Wang <xiaoyu.w.99@gmail.com>
2020-12-20 23:00:26 -0700

    update

--- 2020/2020day10.cpp
+++ 2020/2020day10.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 int solveP1();
-int solveP2();
+long long int solveP2();
 int main()
 {
 
@@ -22,22 +21,45 @@ int solveP1()
         arr.push_back(x);
     f.close();
     sort(arr.begin(), arr.end());
-    int one=0, three=1;
-    if (arr[0]==1)
+    int one = 0, three = 1;
+    if (arr[0] == 1)
         one++;
-    if (arr[0]==3)
+    if (arr[0] == 3)
         three++;
-    for (int i = 0; i < arr.size()-1; i++)
+    for (int i = 0; i < arr.size() - 1; i++)
     {
-        if(arr[i+1]-arr[i]==1)
+        if (arr[i + 1] - arr[i] == 1)
             one++;
-        if(arr[i+1]-arr[i]==3)
+        if (arr[i + 1] - arr[i] == 3)
             three++;
     }
-    return one*three;
+    return one * three;
 }
-int solveP2()
+long long int solveP2()
 {
-
-    return -1;
+    ifstream f("2020day10.txt");
+    int x;
+    vector<int> arr;
+    while (f >> x)
+        arr.push_back(x);
+    f.close();
+    sort(arr.begin(), arr.end());
+    vector<long long int> nodes(arr.size(),1);
+    nodes[nodes.size()-1] = 1;
+    for (int i = nodes.size()-2; i >= 0; i--)
+    {
+        long long int sum=0;
+        for (int j = i+1; j < nodes.size();j++)
+            if (arr[j]-arr[i]<=3)
+                sum += nodes[j];
+        nodes[i] *= sum;
+        //cout << nodes[i] <<endl;
+    }
+    long long int ans = 0;
+    for (int i = 0; i < arr.size();i++)
+        if (arr[i]<=3)
+            ans += nodes[i];
+        else
+            break;
+        return ans;
 }
---
+++ 2020/2020day11.py
@@ -0,0 +1,51 @@
+# Part 1
+
+def p1():
+    seats = []
+    with open("2020day11.txt") as f:
+        seats = f.readlines()
+    for i in range(len(seats)):
+        seats[i] = "-"+seats[i].strip()+"-"
+    seats.insert(0, "-"*len(seats[0]))
+    seats.append("-"*len(seats[0]))
+
+    while(True):
+        temp = seats.copy()
+        for i in range(1, len(seats)-1):
+            for j in range(1, len(seats[i])-1):
+                if seats[i][j] == "L":
+                    c = 0
+                    c += 1 if seats[i][j+1] == "#" else 0
+                    c += 1 if seats[i][j-1] == "#" else 0
+                    c += 1 if seats[i+1][j+1] == "#" else 0
+                    c += 1 if seats[i+1][j-1] == "#" else 0
+                    c += 1 if seats[i-1][j+1] == "#" else 0
+                    c += 1 if seats[i-1][j-1] == "#" else 0
+                    c += 1 if seats[i+1][j] == "#" else 0
+                    c += 1 if seats[i-1][j] == "#" else 0
+                    if (c == 0):
+                        temp[i] = temp[i][:j]+"#"+temp[i][j+1:]
+                elif seats[i][j] == "#":
+                    c = 0
+                    c += 1 if seats[i][j+1] == "#" else 0
+                    c += 1 if seats[i][j-1] == "#" else 0
+                    c += 1 if seats[i+1][j+1] == "#" else 0
+                    c += 1 if seats[i+1][j-1] == "#" else 0
+                    c += 1 if seats[i-1][j+1] == "#" else 0
+                    c += 1 if seats[i-1][j-1] == "#" else 0
+                    c += 1 if seats[i+1][j] == "#" else 0
+                    c += 1 if seats[i-1][j] == "#" else 0
+                    if (c >= 4):
+                        temp[i] = temp[i][:j]+"L"+temp[i][j+1:]
+        if temp == seats:
+            break
+        seats = temp.copy()
+    count = 0
+    for l in seats:
+        count += l.count("#")
+    return count
+
+
+if __name__ == "__main__":
+    print("Part 1 Answer:", p1())
+    # print("Part 2 Answer:", p2())
